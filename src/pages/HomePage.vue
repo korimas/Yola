5c083c88a64ae8dde803c41fcc35b2f322acaa39
Korimas <himoker@163.com>
2024-07-20 19:55:03 +0800

    update

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,28 @@
 <template>
   <q-page class="row items-center justify-center">
     <div class="container">
-      <h1 class="title">Hello, I am YOLA!</h1>
+      <h1 class="title">{{ displayedText }}</h1>
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 defineOptions({
   name: 'HomePage',
 });
+
+let displayedText = ref('');
+let text = 'Hello, I am YOLA!';
+
+let index = 0;
+const timer = setInterval(() => {
+  if (index < text.length) {
+    displayedText.value += text.charAt(index);
+    index++;
+  } else {
+    clearInterval(timer);
+  }
+}, 100);
 </script>
