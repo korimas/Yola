42e67c80b12d17469a977527cc8b99b77c6c1f3d
Korimas <himoker@163.com>
2024-07-28 23:53:39 +0800

    update

--- src/pages/CalculatePage.vue
+++ src/pages/CalculatePage.vue
@@ -23,10 +23,11 @@
       <q-space />
       <div style="margin-top: 66px; margin-bottom: 66px">
         <span style="font-size: 55px"
-          >{{ num1 }} {{ operator }} {{ num2 }} = {{ display_result }}</span
+          >{{ current_quesstion.num1 }} {{ current_quesstion.operator }}
+          {{ current_quesstion.num2 }} = {{ display_result }}</span
         >
         <!-- <q-icon size="md" name="done" /> -->
-        <q-icon v-if="is_wrong" size="md" name="close" color="red" />
+        <q-icon v-if="display_wrong" size="md" name="close" color="red" />
       </div>
       <q-input
         v-model="input_result"
@@ -76,7 +77,7 @@
           :key="item.id"
           v-bind="item"
         >
-          <q-icon v-if="is_wrong" size="md" name="close" color="red" />
+          <q-icon size="md" name="close" color="red" />
           <q-item-section
             >{{ item.num1 }} {{ item.operator }} {{ item.num2 }} =
             {{ item.wrong_answer }}</q-item-section
@@ -130,12 +131,18 @@ defineOptions({
 });
 
 // 计算题组成
-let num1 = ref(0);
-let operator = ref('+');
-let num2 = ref(0);
+let question_index = 0;
+let current_quesstion = ref<QuestionItem>({
+  id: 0,
+  num1: 0,
+  operator: '+',
+  num2: 0,
+  wrong_answer: null,
+  next: null,
+  correct_count: 0,
+});
 let display_result = ref('?'); // 用于显示用户的输入
 const operators = ['+', '-'];
-let result = ref(0); // 正确结果
 let timeoutId: NodeJS.Timeout | undefined; // 检查延时的计时器ID
 let check_delay = 800; // 检查延时(ms)
 
@@ -146,10 +153,9 @@ let input_result = ref<number | null>(null); // 绑定用户输入的input
 let total_count = ref(0); // 总题数
 let correct_count = ref(0); // 答对题数
 let wrong_count = ref(0); // 答错题数
-let same_question = false;
-let is_wrong = false;
-let wrong_list = ref<WrongItem[]>([]);
-let need_retest = [];
+let already_answered = false;
+let display_wrong = false;
+let wrong_list = ref<QuestionItem[]>([]);
 let WrongListDrawerOpen = ref(false);
 
 // 答题计时
@@ -161,56 +167,51 @@ let CalculateSettingDrawerOpen = ref(false);
 let end_number = ref(20);
 
 // 记忆曲线
-// const memoryCurveIntervals = [60000, 150000, 300000]; // 间隔时间 (以毫秒为单位) 模拟记忆曲线
+let need_retest: QuestionItem[] = [];
+let insert_wrong: QuestionItem | null = null;
+const memoryCurveIntervals = [5000, 10000, 30000]; // 间隔时间 (以毫秒为单位) 模拟记忆曲线
 
-interface WrongItem {
+interface QuestionItem {
   id: number;
   num1: number;
   operator: string;
   num2: number;
   wrong_answer: number | null;
+  next: Date | null;
+  correct_count: number; // 答对次数
 }
 
-function generate_new() {
+function generate_question() {
   // 出新题
-  const randomOperator =
-    operators[Math.floor(Math.random() * operators.length)];
-  const num1_t = Math.floor(Math.random() * end_number.value - 1) + 1;
-  const num2_t = Math.floor(Math.random() * end_number.value - 1) + 1;
+  let question: QuestionItem = {
+    id: question_index,
+    num1: Math.floor(Math.random() * (end_number.value - 1)) + 1,
+    operator: operators[Math.floor(Math.random() * operators.length)],
+    num2: Math.floor(Math.random() * (end_number.value - 1)) + 1,
+    wrong_answer: null,
+    next: null,
+    correct_count: 0,
+  };
+  question_index += 1;
 
-  if (randomOperator === '-') {
-    [num1.value, num2.value] = [
-      Math.max(num1_t, num2_t),
-      Math.min(num1_t, num2_t),
+  if (question.operator === '-') {
+    [question.num1, question.num2] = [
+      Math.max(question.num1, question.num2),
+      Math.min(question.num1, question.num2),
     ];
-  } else {
-    [num1.value, num2.value] = [num1_t, num2_t];
   }
-  operator.value = randomOperator;
-  result.value = eval(`${num1.value} ${operator.value} ${num2.value}`);
-}
 
-function generate_wrong() {
-  // 五分之一的概率从错题集中出题
-  if (Math.random() < 0.2 && wrong_list.value.length > 0) {
-    const randomIndex = Math.floor(Math.random() * wrong_list.value.length);
-    const randomWrongItem = wrong_list.value[randomIndex];
-    const [num1_t, operator_t, num2_t] = [
-      randomWrongItem.num1,
-      randomWrongItem.operator,
-      randomWrongItem.num2,
-    ];
-    num1.value = num1_t;
-    operator.value = operator_t;
-    num2.value = num2_t;
-    result.value = eval(`${num1.value} ${operator.value} ${num2.value}`);
-    return;
-  }
+  return question;
 }
 
 function generate() {
-  generate_wrong();
-  generate_new();
+  // generate_wrong();
+  if (insert_wrong !== null) {
+    current_quesstion.value = insert_wrong;
+    return;
+  } else {
+    current_quesstion.value = generate_question();
+  }
 }
 
 // 计时器
@@ -222,43 +223,103 @@ setInterval(() => {
   seconds.value += 1;
 }, 1000);
 
+// 记忆曲线定时检查
+setInterval(() => {
+  if (need_retest.length <= 0) {
+    return;
+  }
+
+  need_retest.forEach((item) => {
+    if (insert_wrong !== null) {
+      return;
+    }
+
+    if (item.next !== null && item.next <= new Date()) {
+      console.log('找到需要复习的题目：' + item.id);
+      insert_wrong = item;
+      return;
+    }
+  });
+}, 3000);
+
 // 延时检查输入答案
 function checkResult() {
-  if (input_result.value !== null) {
-    display_result.value = input_result.value.toString();
-  } else {
-    display_result.value = '?';
-  }
-  is_wrong = false;
+  // 更新显示的值
+  display_result.value =
+    input_result.value !== null ? input_result.value.toString() : '?';
 
+  // 清除错误图标
+  display_wrong = false;
+
+  // 清除上一个检查延时的计时器
   if (timeoutId) {
     clearTimeout(timeoutId);
   }
 
+  // 设置新的检查延时的计时器
   timeoutId = setTimeout(() => {
-    if (Number(input_result.value) === result.value) {
-      generate();
-      if (!same_question) {
+    //  计算正确答案
+    let question = current_quesstion.value;
+    let correct_answer: number = eval(
+      `${question.num1} ${question.operator} ${question.num2}`
+    );
+    if (Number(input_result.value) === correct_answer) {
+      // 答对了
+      if (question.next !== null && !already_answered) {
+        // 当前是错题
+        question.correct_count += 1;
+        if (question.correct_count >= memoryCurveIntervals.length) {
+          // 已经做完记忆曲线
+          need_retest = need_retest.filter((item) => item.id != question.id);
+        } else {
+          // 等待下一次记忆时刻
+          question.next = new Date(
+            Date.now() + memoryCurveIntervals[question.correct_count]
+          );
+          console.log(
+            '答对错题：' + question.id + '，下次复习时间：' + question.next
+          );
+          insert_wrong = null;
+        }
+      }
+
+      // 统计
+      if (!already_answered) {
         correct_count.value += 1;
       }
       total_count.value += 1;
-      same_question = false;
+      already_answered = false;
       display_result.value = '?';
+
+      // 出新题
+      generate();
     } else {
-      if (!same_question) {
-        wrong_count.value += 1;
+      // 答错了
+      if (question.next !== null) {
+        // 当前是错题
+        question.correct_count = 0;
+        question.next = new Date(Date.now() + memoryCurveIntervals[0]);
+        console.log(
+          '答错错题：' + question.id + '，下次复习时间：' + question.next
+        );
+        insert_wrong = null;
       }
-      same_question = true;
-      is_wrong = true;
-      let wrong_item = {
-        id: wrong_list.value.length + 1,
-        num1: num1.value,
-        num2: num2.value,
-        operator: operator.value,
-        wrong_answer: input_result.value,
-      };
-      wrong_list.value.push(wrong_item);
-      need_retest.push(wrong_item);
+
+      if (!already_answered) {
+        wrong_count.value += 1;
+
+        let wrong_item = current_quesstion.value;
+        wrong_item.wrong_answer = input_result.value;
+        wrong_item.correct_count = 0;
+        wrong_item.next = new Date(Date.now() + memoryCurveIntervals[0]);
+        wrong_list.value.push(wrong_item);
+        need_retest.push(wrong_item);
+        console.log(
+          '增加错题：' + question.id + '，下次复习时间：' + question.next
+        );
+      }
+      already_answered = true;
+      display_wrong = true;
     }
     input_result.value = null;
   }, check_delay);
